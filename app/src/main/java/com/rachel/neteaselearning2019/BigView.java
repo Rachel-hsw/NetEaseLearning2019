8327dc13b6d442f8097a944c896ecbcbacf9f925
Rachel <1623404291@qq.com>
2019-11-01 13:34:42 +0800

    Android大长图如何加载及其原理

---
+++ app/src/main/java/com/rachel/neteaselearning2019/BigView.java
@@ -0,0 +1,19 @@
+package com.rachel.neteaselearning2019;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class BigView extends View {
+    public BigView(Context context) {
+        super(context);
+    }
+
+    public BigView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BigView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
